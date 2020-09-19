23343d7ccdf03127b29034bf23640e5dfe7f0a44
Moose Davis <moosedavis2011@gmail.com>
2020-09-19 00:57:56 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -6,8 +6,27 @@ class Song
     @@all
   end
 
+  def self.create
+    self.new.tap{|s| s.save}
+  end 
+
   def save
     self.class.all << self
   end
+  
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end 
+  
+  def self.create_by_name(name)
+    song = self.new.tap{|s| s.save}
+    song.name = name
+    song
+  end 
 
+  def self.find_by_name(name)
+      self.all.find{|song| song.name == name}
+  end 
 end
